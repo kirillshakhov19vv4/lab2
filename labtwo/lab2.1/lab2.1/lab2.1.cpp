e827f7643ee3a59b029a6ff5a2162b37401c48eb
kirillshakhov19vv4 <72416705+kirillshakhov19vv4@users.noreply.github.com>
2020-12-16 15:08:30 +0300

    Add files via upload

---
+++ labtwo/lab2.1/lab2.1/lab2.1.cpp
@@ -0,0 +1,167 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <conio.h> 
+#include <iostream> 
+#include <locale.h>
+
+using namespace std;
+
+clock_t start, stop;
+
+int CLOCK_PER_MS = CLOCKS_PER_SEC / 1000;
+
+void shell(int* items, int count)
+{
+
+	int i, j, gap, k;
+	int x, a[5];
+
+	a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+
+	for (k = 0; k < 5; k++) {
+		gap = a[k];
+		for (i = gap; i < count; ++i) {
+			x = items[i];
+			for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+				items[j + gap] = items[j];
+			items[j + gap] = x;
+		}
+	}
+}
+
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1); 
+{
+	int i, j;
+	int x, y;
+
+	i = left; j = right;
+
+	/* выбор компаранда */
+	x = items[(left + right) / 2];
+
+	do {
+		while ((items[i] < x) && (i < right)) i++;
+		while ((x < items[j]) && (j > left)) j--;
+
+		if (i <= j) {
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++; j--;
+		}
+	} while (i <= j);
+
+	if (left < j) qs(items, left, j);
+	if (i < right) qs(items, i, right);
+}
+
+int compare(const void* a, const void* b)
+{
+	return (*(int*)a - *(int*)b);
+}
+
+int* copy_list(int* arr, int count) {
+	int* new_list = (int*)malloc(count * (sizeof(int)));
+	for (int i = 0; i < count; i++) {
+		new_list[i] = arr[i];
+	}
+	return new_list;
+}
+
+void del_list(int* arr) {
+	free(arr);
+}
+
+void fillArray(int** arr, int count) {
+	for (int i = 0; i < count; i++) {
+		arr[0][i] = -1000 + rand() % 2000;
+	}
+	for (int i = 0; i < count; i++) {
+		arr[1][i] = i * 2 + 14;
+	}
+	for (int i = 0; i < count; i++) {
+		arr[2][i] = arr[1][count - i];
+	}
+	for (int i = 0; i < count; i++) {
+		switch (i / 2) {
+		case 0:
+			arr[3][i] = i * 2 + 14;
+			break;
+		case 1:
+			arr[3][count - i - 1] = i * 2 + 14;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void sorting_set(int** set, int count, FILE* file) {
+	int* spisok;
+	for (int i = 0; i < 4; i++) {
+		printf(".");
+		fprintf(file, "%d. ", i + 1);
+		spisok = copy_list(set[i], count);
+		start = clock() / CLOCK_PER_MS;
+		shell(spisok, count);
+		stop = clock() / CLOCK_PER_MS;
+		del_list(spisok);
+		fprintf(file, "%dms  ", (stop - start));
+
+		spisok = copy_list(set[i], count);
+		start = clock() / CLOCK_PER_MS;
+		qs(spisok, 0, count - 1);
+		stop = clock() / CLOCK_PER_MS;
+		del_list(spisok);
+		fprintf(file, "%dms  ", (stop - start));
+
+		spisok = copy_list(set[i], count);
+		start = clock() / CLOCK_PER_MS;
+		qsort(spisok, count, sizeof(int), compare);
+		stop = clock() / CLOCK_PER_MS;
+		del_list(spisok);
+		fprintf(file, "%dms  \n", (stop - start));
+	}
+}
+
+
+
+void main(void)
+{
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел 
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setlocale(LC_ALL, "Rus");
+
+	FILE* file;
+	char name[] = "logs.txt";
+	if ((file = fopen(name, "w")) == NULL) {
+		printf("Не удалось открыть файл");
+		_getch();
+	}
+	int sizes[5] = { 10000, 20000, 40000, 80000, 160000 };
+	int pointer_size = 0;
+	int** matrix;
+	printf("waiting");
+	fprintf(file, "Наборы данных:\n1.Рандомный\n2.Возрастающий\n3.Убывающий\n4.Первая половина возрастает, вторая - убывает.\n");
+	while (pointer_size < 5) {
+		matrix = (int**)malloc(4 * sizeof(int*));
+		for (int i = 0; i < 4; i++) {
+			matrix[i] = (int*)malloc((sizes[pointer_size] * sizeof(int)));
+		}
+		fillArray(matrix, sizes[pointer_size]);
+		fprintf(file, "\nНабор данных размером в %d элементов\nshell || qs || qsort\n", sizes[pointer_size]);
+		sorting_set(matrix, sizes[pointer_size], file);
+		pointer_size++;
+		for (int i = 0; i < 4; i++)
+		{
+			free(matrix[i]);
+		}
+		free(matrix);
+
+	}
+	printf("\ndone");
+	fclose(file);
+}
---
+++ labtwo/lab2/lab2/lab2.cpp
@@ -0,0 +1,93 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <conio.h> 
+#include <iostream> 
+#include <locale.h>
+
+using namespace std;
+
+int CLOCK_PER_MS = CLOCKS_PER_SEC / 1000;
+
+clock_t start, stop;
+
+int** fillMatrix(int size) {
+	int i = 0, j = 0;
+	int** a = (int**)malloc(size * sizeof(int*));
+	for (int k = 0; k < size; k++) {
+		a[k] = (int*)malloc(size * sizeof(int));
+	}
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	while (i < size)
+	{
+		while (j < size)
+		{
+			a[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
+			j++;
+		}
+		i++;
+	}
+	return a;
+}
+
+int** matrix_multiplication(int** a, int** b, int size)
+{
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	int i = 0, j = 0, r;
+	int  elem_c;
+
+	int** c = (int**)malloc(size * sizeof(int*));
+	for (int k = 0; k < size; k++) {
+		c[k] = (int*)malloc(size * sizeof(int));
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			elem_c = 0;
+			for (r = 0; r < size; r++)
+			{
+				elem_c = elem_c + a[i][r] * b[r][j];
+				c[i][j] = elem_c;
+			}
+		}
+	}
+	return(c);
+}
+
+
+void main() {
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел 
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setlocale(LC_ALL, "Rus");
+	int** a;
+	int** b;
+	int** c;
+
+	int sizes[] = { 100, 200, 250, 300, 350, 400};
+	int times[6] = { 0 };
+	printf("Время выполнения операции (в мс) в зависимости от размера:\n");
+	for (int i = 0; i < 6; i++) {
+		a = fillMatrix(sizes[i]);
+		b = fillMatrix(sizes[i]);
+		start = clock();
+		c = matrix_multiplication(a, b, sizes[i]);
+		stop = clock();
+		times[i] = (stop - start) / CLOCK_PER_MS;
+		printf("%d элементов - %d ms\n", sizes[i], times[i]);
+		for (int i = 0; i < 4; i++)
+		{
+			free(a[i]);
+			free(b[i]);
+			free(c[i]);
+		}
+		free(a);
+		free(b);
+		free(c);
+	}
+}
